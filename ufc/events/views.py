94384a9a3773de75c6b079afe6e602ff94656cb2
AlexSelby1 <alex.selby@hotmail.com>
2021-05-14 09:43:43 +0100

    Registered Fight and added to views

--- ufc/events/views.py
+++ ufc/events/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Event
+from .models import Fight
 
 # Create your views here.
 
 # View to display the upcoming event
 def future_event(request):
     futureEvent = Event.objects.is_next()
+    for event in futureEvent:
+        eventFights = event.fights.all()
     return render(request,
                 'future_event.html',
-                {'futureEvent': futureEvent})
+                {'futureEvent': futureEvent,
+                 'eventFights': eventFights})
 
 # View to display the list of past events
 def past_events_list(request):
@@ -19,9 +23,9 @@ def past_events_list(request):
 
 # View to display the past event by id
 def past_event_details(request, id):
-    pastEvent = Event.objects.is_past()
-    pastEvent = get_object_or_404(Event,
-                                id=id)
+    pastEvent = get_object_or_404(Event, id=id)
+    eventFights = pastEvent.fights.all()
     return render(request,
                 'past_event_details.html',
-                {'event': pastEvent})
+                {'pastEvent': pastEvent,
+                 'eventFights': eventFights})
